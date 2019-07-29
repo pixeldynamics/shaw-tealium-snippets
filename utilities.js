53f4b3b3086d2c46684caef5e18d670cd236c8b5
Mike <mike.barkemeyer@sjrb.ca>
2019-07-29 10:21:38 -0600

    Device Detection

--- utilities.js
+++ utilities.js
@@ -114,6 +114,38 @@ try {
 catch(e) {
   //Error!
 }
+//Device Detection
+try {
+    window.deviceDetector = (function () {
+        var ua = navigator.userAgent.toLowerCase();
+        var detect = (function(s) {
+            if(s === undefined) { s = ua; }
+            else { ua = s.toLowerCase(); }
+
+            if(/(ipad|tablet|(android(?!.*mobile))|(windows(?!.*phone)(.*touch))|kindle|playbook|silk|(puffin(?!.*(IP|AP|WP))))/.test(ua)) {
+                return 'tablet';
+            }
+
+            else {
+                if(/(mobi|ipod|phone|blackberry|opera mini|fennec|minimo|symbian|psp|nintendo ds|archos|skyfire|puffin|blazer|bolt|gobrowser|iris|maemo|semc|teashark|uzard)/.test(ua)) {
+                    return 'phone';
+                }
+                else {
+                    return 'desktop';
+                }
+            }
+        });
+        return {
+            device:detect(),
+            detect:detect,
+            isMobile:((detect()!='desktop')?true:false),
+            userAgent:ua
+        };
+    }());
+}
+catch (e) {
+    /* Unable to detect device */
+}
 
 /*
 Wait Until Exists
