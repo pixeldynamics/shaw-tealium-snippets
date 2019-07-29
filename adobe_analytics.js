3c443e50e62efc86121317dbdc7cf2cd690d228f
Mike <mike.barkemeyer@sjrb.ca>
2019-07-29 11:00:02 -0600

    Combine Adobe Functions

---
+++ adobe_analytics.js
@@ -0,0 +1,219 @@
+//Do Plugins
+s.usePlugins = true;
+if (typeof(Visitor) != 'undefined') {
+  s.visitor = Visitor.getInstance("5F34123F5245B4A70A490D45@AdobeOrg");
+}
+function s_doPlugins(s) {
+
+    /*SITE CONTENT EFFECTIVENESS*/
+
+    /*Capturing URL */
+    s.eVar10 = window.location.toString();
+    /*Copying Variables */
+    if (s.eVar10) {
+        s.prop10 = s.eVar10;
+        s.prop3 =s.eVar3 =  s.getPreviousValue(s.prop10, 'p_url', ''); // no value will return until s.prop10 is populated
+    }
+
+    /*Capturing User Agent String*/
+    s.eVar9 = window.navigator.userAgent;
+    /*Copying Variables */
+    if (s.eVar9)
+        s.prop9 = s.eVar9;
+
+    /*Capturing New/Repeat Visitor*/
+    s.eVar4 = s.getNewRepeat(60);
+    /*Copying Variables */
+    if (s.eVar4)
+        s.prop4 = s.eVar4;
+
+    /* Capturing version of s_code*/
+    s.eVar11 = s.version;
+    /*Copying Variables*/
+    if (s.eVar11)
+        s.prop11 = s.eVar11;
+
+
+    /* For Previous Page value */
+    if (s.pageName)
+        s.prop18 = s.getPreviousValue(s.pageName, 'gpv', ''); // no value will return until s.pageName is populated
+
+    /*capturing the percent of page viewed*/
+    var ppv = s.getPercentPageViewed(s.pageName); // no value will return until s.pageName is populated
+    s.prop16 = ppv[0] + '|' + ppv[1] + '|' + ppv[2];
+
+
+    /*Page number in a visit*/
+    s.prop17 = s.getActionDepth('s_depth');
+
+    b.marketing_cloud_id = s.eVar13 = s.visitor.getMarketingCloudVisitorID();
+    if (!s.eVar13){
+    b.marketing_cloud_id = s.eVar13 = s.c_r('AMCV_5F34123F5245B4A70A490D45@AdobeOrg').replace(/.*MCMID\ |-( [^\|]*)\|.*/g,"$1");
+    }
+}
+s.doPlugins = s_doPlugins
+
+//Config
+/*
+ * Plugin Utility: Append to List v1.2
+ */
+s.apl=new Function("l","v","d","u",""
++"var s=this,m=0;if(!l)l='';if(u){var i,n,a=l.split(d),al=a.length;fo"
++"r(i=0;i<al;i++){n=a[i];m=m||(u==1?(n==v):(n.toLowerCase()==v.toLowe"
++"rCase()));}}if(!m)l=l?l+d+v:v;return l;");
+
+ /*
+ * Utility Function: split v1.5 (JS 1.0 compatible)
+ */
+s.split=new Function("l","d",""
++"var i,x=0,a=new Array;while(l){i=l.indexOf(d);i=i>-1?i:l.length;a[x"
++"++]=l.substring(0,i);l=l.substring(i+d.length);}return a");
+
+/*
+ * Plugin: getNewRepeat 1.2 - Returns whether user is new or repeat
+ */
+s.getNewRepeat=new Function("d","cn",""
++"var s=this,e=new Date(),cval,sval,ct=e.getTime();d=d?d:30;cn=cn?cn:"
++"'s_nr';e.setTime(ct+d*24*60*60*1000);cval=s.c_r(cn);if(cval.length="
++"=0){s.c_w(cn,ct+'-New',e);return'New';}sval=s.split(cval,'-');if(ct"
++"-sval[0]<30*60*1000&&sval[1]=='New'){s.c_w(cn,ct+'-New',e);return'N"
++"ew';}else{s.c_w(cn,ct+'-Repeat',e);return'Repeat';}");
+
+
+/*
+ * Plugin: getTimeParting 3.4
+ */
+s.getTimeParting=new Function("h","z",""
++"var s=this,od;od=new Date('1/1/2000');if(od.getDay()!=6||od.getMont"
++"h()!=0){return'Data Not Available';}else{var H,M,D,U,ds,de,tm,da=['"
++"Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturda"
++"y'],d=new Date();z=z?z:0;z=parseFloat(z);if(s._tpDST){var dso=s._tp"
++"DST[d.getFullYear()].split(/,/);ds=new Date(dso[0]+'/'+d.getFullYea"
++"r());de=new Date(dso[1]+'/'+d.getFullYear());if(h=='n'&&d>ds&&d<de)"
++"{z=z+1;}else if(h=='s'&&(d>de||d<ds)){z=z+1;}}d=d.getTime()+(d.getT"
++"imezoneOffset()*60000);d=new Date(d+(3600000*z));H=d.getHours();M=d"
++".getMinutes();M=(M<10)?'0'+M:M;D=d.getDay();U=' AM';if(H>=12){U=' P"
++"M';H=H-12;}if(H==0){H=12;}D=da[D];tm=H+':'+M+U;return(tm+'|'+D);}");
+
+/*
+ * Plugin: getPercentPageViewed v1.74
+ */
+s.getPercentPageViewed=new Function("n",""
++"var s=this,W=window,EL=W.addEventListener,AE=W.attachEvent,E=['load"
++"','unload','scroll','resize','zoom','keyup','mouseup','touchend','o"
++"rientationchange','pan'],K='s_ppv',P=K+'l',I=n||s.pageName||documen"
++"t.location.href;W.s_Obj=s;if(!W.s_PPVevent){s.s_PPVg=function(n,o){"
++"var c=s.c_r(o?P:K)||'',a=c.indexOf(',')>-1?c.split(',',10):[''],i;a"
++"[0]=o?unescape(a[0]||''):I;for(i=1;i<9&&(i<a.length||!o);i++)a[i]=a"
++"[i]?parseInt(a[i])||0:0;if(a.length>9||!o)a[9]=a[9]&&a[9]!='L'&&a[9"
++"]!='LP'&&a[9]!='PL'?'P':a[9];return a};s.c_w(P,s.c_r(K)||'');s.c_w("
++"K,escape(I)+',0,0,0,0,0,0,0,0');W.s_PPVevent=function(e){var W=wind"
++"ow,D=document||{},B=D.body,E=D.documentElement||{},S=window.screen|"
++"|{},Ho='offsetHeight',Hs='scrollHeight',Ts='scrollTop',Wc='clientWi"
++"dth',Hc='clientHeight',M=Math,C=100,J='object',N='number',Z=',',s=W"
++".s_Obj||W.s||0;e=e&&typeof e==J?e.type||'':'';if(!e.indexOf('on'))e"
++"=e.substring(2);if(W.s_PPVt&&!e){clearTimeout(s_PPVt);s_PPVt=0}if(s"
++"&&typeof s==J&&B&&typeof B==J){var h=M.max(B[Hs]||E[Hs],B[Ho]||E[Ho"
++"],B[Hc]||E[Hc]||1),X=W.innerWidth||E[Wc]||B[Wc]||1,Y=W.innerHeight|"
++"|E[Hc]||B[Hc]||1,x=S.width||1,y=S.height||1,r=M.round(C*(W.devicePi"
++"xelRatio||1))/C,b=(D.pageYOffset||E[Ts]||B[Ts]||0)+Y,p=h>0&&b>0?M.r"
++"ound(C*b/h):1,O=W.orientation,o=!isNaN(O)?M.abs(O)%180:Y>X?0:90,a=s"
++".s_PPVg(n),L=(e=='load')||(a[1]<1),t,V=function(u,v,f,n){v=typeof v"
++"!=N?u:v;v=f||(u>v)?u:v;return n?v:v>C?C:v<0?0:v};if(new RegExp('(iP"
++"od|iPad|iPhone)').exec((window.navigator&&navigator.userAgent)||'')"
++"&&o){t=x;x=y;y=t}o=o?'L':'P';a[9]=L||!a[9]?o:a[9].substring(0,1);if"
++"(a[9]!='L'&&a[9]!='P')a[9]=o;s.c_w(K,escape(a[0])+Z+V(a[1],p,!L)+Z+"
++"V(a[2],p,L)+Z+V(a[3],b,L,1)+Z+X+Z+Y+Z+x+Z+y+Z+r+Z+a[9]+(a[9]==o?'':"
++"o))}if(!W.s_PPVt&&e!='unload')W.s_PPVt=setTimeout(W.s_PPVevent,333)"
++"};for(var f=W.s_PPVevent,i=0;i<E.length;i++)if(EL)EL(E[i],f,false);"
++"else if(AE)AE('on'+E[i],f);f()};var a=s.s_PPVg(n,1);return!argument"
++"s.length||n=='-'?a[1]:a");
+
+/*
+ * Plugin: getPreviousValue v1.0 - return previous value of designated
+ *   variable (requires split utility)
+ */
+s.getPreviousValue=new Function("v","c","el",""
++"var s=this,t=new Date,i,j,r='';t.setTime(t.getTime()+1800000);if(el"
++"){if(s.events){i=s.split(el,',');j=s.split(s.events,',');for(x in i"
++"){for(y in j){if(i[x]==j[y]){if(s.c_r(c)) r=s.c_r(c);v?s.c_w(c,v,t)"
++":s.c_w(c,'no value',t);return r}}}}}else{if(s.c_r(c)) r=s.c_r(c);v?"
++"s.c_w(c,v,t):s.c_w(c,'no value',t);return r}");
+
+/*
+ *  Plug-in: crossVisitParticipation v1.7 - stacks values from
+ *  specified variable in cookie and returns value
+ */
+
+s.crossVisitParticipation=new Function("v","cn","ex","ct","dl","ev","dv",""
++"var s=this,ce;if(typeof(dv)==='undefined')dv=0;if(s.events&&ev){var"
++" ay=s.split(ev,',');var ea=s.split(s.events,',');for(var u=0;u<ay.l"
++"ength;u++){for(var x=0;x<ea.length;x++){if(ay[u]==ea[x]){ce=1;}}}}i"
++"f(!v||v==''){if(ce){s.c_w(cn,'');return'';}else return'';}v=escape("
++"v);var arry=new Array(),a=new Array(),c=s.c_r(cn),g=0,h=new Array()"
++";if(c&&c!=''){arry=s.split(c,'],[');for(q=0;q<arry.length;q++){z=ar"
++"ry[q];z=s.repl(z,'[','');z=s.repl(z,']','');z=s.repl(z,\"'\",'');arry"
++"[q]=s.split(z,',')}}var e=new Date();e.setFullYear(e.getFullYear()+"
++"5);if(dv==0&&arry.length>0&&arry[arry.length-1][0]==v)arry[arry.len"
++"gth-1]=[v,new Date().getTime()];else arry[arry.length]=[v,new Date("
++").getTime()];var start=arry.length-ct<0?0:arry.length-ct;var td=new"
++" Date();for(var x=start;x<arry.length;x++){var diff=Math.round((td."
++"getTime()-arry[x][1])/86400000);if(diff<ex){h[g]=unescape(arry[x][0"
++"]);a[g]=[arry[x][0],arry[x][1]];g++;}}var data=s.join(a,{delim:',',"
++"front:'[',back:']',wrap:\"'\"});s.c_w(cn,data,e);var r=s.join(h,{deli"
++"m:dl});if(ce)s.c_w(cn,'');return r;");
+
+/*
+ * Plugin: getActionDepth v1.0 - Returns the current
+ * page number of the visit
+ */
+s.getActionDepth=new Function("c",""
++ "var s=this,v=1,t=new Date;t.setTime(t.getTime()+1800000);"
++ "if(!s.c_r(c)){v=1}if(s.c_r(c)){v=s.c_r(c);v++}"
++ "if(!s.c_w(c,v,t)){s.c_w(c,v,0)}return v;");
+
+
+//Pageview - Event1, Link Click - Event12
+if (a == 'view'){
+    /*b.page_load_flag = "true";*/
+    u.addEvent("event1");
+  } else if (a == 'link'){
+    /*b.page_load_flag = "false";*/
+    u.addEvent("event12");
+  }
+
+//Parse Timestamp (TODO: Make time to fix this old junk)
+var days = {'sun':'sunday'
+           ,'mon':'monday'
+	   ,'tue':'tuesday'
+	   ,'wed':'wednesday'
+	   ,'thu':'thursday'
+	   ,'fri':'friday'
+	   ,'sat':'saturday'
+	   };
+var time_list = b.timestamp.split(' ');
+//grab day and time dynamically instead of hardcoding the list index
+for(var i=0; i<time_list.length; i++) {
+  if(time_list[i] in days)
+    var in_day = time_list[i];
+  if(time_list[i].indexOf(':')>0)
+    var in_time = time_list[i];
+}
+var hour = parseInt(in_time.split(':')[0]);
+var ampm = 'am';
+
+if(hour==24) {
+  hour = 12;
+  ampm = 'am';
+}
+else if(hour==12) {
+  hour = 12;
+  ampm = 'pm';
+}
+else if(hour>12) {
+  hour = hour-12;
+  ampm = 'pm';
+}
+
+var out_time = hour+':'+in_time.split(':')[1];
+b.hour_of_day = days[in_day]+'|'+out_time+ampm;
