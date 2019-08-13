ef8ca9eb3a1b58b1f8b2c0893f6c190631a8e41b
Mike <mike.barkemeyer@sjrb.ca>
2019-08-13 10:07:00 -0600

    Update to use b. in order utils

--- utilities.js
+++ utilities.js
@@ -258,37 +258,38 @@ window.clickTrackEvent = function(el) {
 
 //Order Data Utilities
 function order_total() {
-    var total = 0, sale_price = utag_data.product_sale_price || [];
+    var total = 0, sale_price = utag_data.product_sale_price || b.product_sale_price || [];
 	sale_price.forEach(function(value) { total += Number(value); });
   	return total;
 }
 function order_id() {
-    return utag_data.order_id || false;
+    return utag_data.order_id || b.order_id || false;
 }
 function order_currency() {
-    return utag_data.order_currency || 'CAD';
+    return utag_data.order_currency || b.order_currency || 'CAD';
 }
 function rgu_count() {
-	var total = 0, rgus = utag_data.product_rgu || [];
+	var total = 0, rgus = utag_data.product_rgu || b.product_rgu || [];
 	rgus.forEach(function(value) { total += !isNaN(value) ? Number(value) : 0 });
   	return total;
 }
 function product_names() {
-    var product_names = utag_data.product_name || [];
+    var product_names = utag_data.product_name || b.product_name || [];
     return product_names.join(',') || false;
 }
 function product_order_types() {
-    var order_type = utag_data.product_order_type || [];
+    var order_type = utag_data.product_order_type || b.product_order_type || [];
     return order_type.join(',') || false;
 }
 function lead_form_name() {
-    return utag_data.lead_form_name || false;
+    return utag_data.lead_form_name || b.lead_form_name || false;
 }
 function lead_form_name_clean() {
-    return utag_data.lead_form_name.replace(/[^a-zA-Z 0-9]+/g, '.') || false;
+    var name = utag_data.lead_form_name || b.lead_form_name
+    return name.replace(/[^a-zA-Z 0-9]+/g, '.') || false;
 }
 function eoid() {
-    return utag_data['qp.eoId'] || false;
+    return utag_data['qp.eoId'] || b.['qp.eoId'] || false;
 }
 function is_thanks() {
   if (order_id() !== false || (/thankyou/i).test(utag_data.form_step)) { return true; }
